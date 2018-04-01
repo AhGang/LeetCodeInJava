50853552715873d3f3c57240e9d6626fe5bfcd92
AhGang <474494594@qq.com>
2018-04-01 09:46:04 +0800

    有效的字母异位词

---
+++ src/IsAnagram.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+
+public class IsAnagram {
+
+public static boolean isAnagram(String s, String t) {
+
+        int[] hash = new int[26];
+        for(int i = 0;i<hash.length;i++){
+        	 System.out.println(hash[i]);
+        }
+        for(int i = 0;i<s.length();i++){
+       	hash[s.charAt(i)-'a']++;	
+        }
+        for(int i = 0;i<t.length();i++){
+        	        	 hash[t.charAt(i)-'a']--;
+        }
+       
+        for(int i = 0;i<hash.length;i++){
+        	if(hash[i]!=0){
+        		
+        		return false;
+        	}
+        }
+       // for (int i : hash) if (i != 0) return false;
+        
+
+       return true;
+    }
+public static void main(String args[]){
+	Scanner sc  = new Scanner(System.in);
+	String s = sc.nextLine();
+	String t = sc.nextLine();
+
+		System.out.println(isAnagram(s,t));
+		
+	
+	sc.close();
+	
+}
+}
